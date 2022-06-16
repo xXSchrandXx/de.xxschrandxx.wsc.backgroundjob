7be01291e1aac0ba9b8319f4e12bebcad45797fa
xXSchrandXx <marvinschrand@yahoo.de>
2022-06-16 22:13:05 +0200

    Add Object decoding

--- files/lib/data/backgroundjob/Backgroundjob.class.php
+++ files/lib/data/backgroundjob/Backgroundjob.class.php
@@ -5,6 +5,8 @@ namespace wcf\data\backgroundjob;
 use wcf\data\DatabaseObject;
 use wcf\system\background\BackgroundQueueHandler;
 use wcf\system\background\job\AbstractBackgroundJob;
+use wcf\util\JSON;
+use wcf\util\StringUtil;
 
 class Backgroundjob extends DatabaseObject
 {
@@ -25,12 +27,12 @@ class Backgroundjob extends DatabaseObject
      * time INT(10) $time
      */
 
-     /**
-      * Gets unserialize backgroundjob
-      * @return ?AbstractBackgroundJob
-      */
-     public function getUnserialized()
-     {
+    /**
+     * Gets unserialize backgroundjob
+     * @return ?AbstractBackgroundJob
+     */
+    public function getUnserialized()
+    {
         $unserializedJob = null;
         try {
             $unserializedJob = \unserialize($this->job);
@@ -38,13 +40,39 @@ class Backgroundjob extends DatabaseObject
             \wcf\functions\exception\logThrowable($e);
         }
         return $unserializedJob;
-     }
+    }
 
-     /**
-      * Executes the job and deletes on success.
-      */
-     public function execute()
-     {
+    /**
+     * Gets the AbstractBackgroundJob`s data
+     * @return array
+     */
+    public function getObjectVars()
+    {
+        $arr = get_object_vars($this->getUnserialized());
+        foreach ($arr as $key => $value) {
+            // Check key
+            if ($key == '__PHP_Incomplete_Class_Name') {
+                $arr['Name'] = $arr[$key];
+                unset($arr[$key]);
+            } else if (preg_replace('/[[:^print:]]/', '', $key) == 'wcf\system\background\job\AbstractBackgroundJobfailures') {
+                $arr['failures'] = $arr[$key];
+                unset($arr[$key]);
+            }
+            // Check value
+            if ($value === null) {
+                $arr[$key] = 'null';
+            } else if (is_array($value)) {
+                $arr[$key] = JSON::encode($value);
+            }
+        }
+        return $arr;
+    }
+
+    /**
+     * Executes the job and deletes on success.
+     */
+    public function execute()
+    {
         try {
             $unserializedJob = $this->getUnserialized();
             if ($unserializedJob) {
@@ -56,6 +84,5 @@ class Backgroundjob extends DatabaseObject
             $editor = new BackgroundjobEditor($this);
             $editor->delete();
         }
-     }
-
+    }
 }
